9cadccc861120441db47a402680b7944f9b6395f
Mahesh Chandra Bhatt <maheshbhatt2205@gmail.com>
2026-05-05 22:45:38 +0530

    Inheritance concept-single inheritance

---
+++ inheritance.py
@@ -0,0 +1,11 @@
+class Animal:
+    def speak(self):
+        print("animal is speaking")
+class Dog(Animal):
+    def bark(self):
+        print("dog is barking")
+d=Dog()
+d.speak()
+d.bark()
+
+              
